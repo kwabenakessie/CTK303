bf9806971ee5a8de667e40993d90f291a1eff6cc
kwabenakessie <70108933+kwabenakessie@users.noreply.github.com>
2023-09-06 02:48:21 -0500

    Assignment 2

--- Assignment1/birthday.js
+++
@@ -1,8 +0,0 @@
-<script>
-    let play = document.getElementById("play");
-        function playMusic() {
-         let audio = new Audio("audio.mp3");
-         audio.play()
-      }
-      play.addEventListener("click", playMusic);
-</script> 
